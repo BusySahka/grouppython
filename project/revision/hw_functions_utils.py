914f4dd9a1cfdf163219c5fd5d7a3449a1eebf46
Your Name <you@example.com>
2025-09-15 19:58:11 +0300

    Homework_logging

---
+++ project/revision/hw_functions_utils.py
@@ -0,0 +1,33 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("app")
+
+
+def return_hello_buddy() -> str:
+    result: str = "Hello, buddy"
+    logger.info(f"return_hello_buddy -> {result}")
+    return result
+
+
+def return_biggest_num_from_array(arr: list) -> float:
+    if not arr:
+        logger.info("return_biggest_num_from_array: array is empty -> 0")
+        return 0
+
+    biggest_num: float = arr[0]
+
+    for elem in arr:
+        if isinstance(elem, (int, float)):
+            if elem > biggest_num:
+                biggest_num = elem
+
+    logger.info(f"return_biggest_num_from_array({arr}) -> {biggest_num}")
+    return biggest_num
+
+
+def multiply_two_numbers(number_1: float, number_2: float) -> float:
+    result: float = number_1 * number_2
+    logger.info(f"multiply_two_numbers({number_1=}, {number_2=}) -> {result}")
+    return result
+
--- project/revision/main.py
+++ project/revision/main.py
@@ -1,18 +1,19 @@
 from function import is_number_bigger_then_given
 from project.revision.function import add_salt_to_list
-
+from hw_functions_utils import return_hello_buddy, return_biggest_num_from_array, multiply_two_numbers
 
 def main():
-    result = is_number_bigger_then_given(candidate_number=5)
-    print(result)
-
-    result = is_number_bigger_then_given(candidate_number=5, theshold=1)
-    print(result)
-
-    given_list = []
-    add_salt_to_list(given_list)
-    add_salt_to_list(given_list)
-    print(given_list)
+    #result = is_number_bigger_then_given(candidate_number=5)
+    #print(result)
+    #result = is_number_bigger_then_given(candidate_number=5, theshold=1)
+    #print(result)
+    #given_list = []
+    #add_salt_to_list(given_list)
+    #add_salt_to_list(given_list)
+    #print(given_list)
+    print(return_hello_buddy())
+    print(return_biggest_num_from_array([10, "a", 5.5, None, 22, 5, "d"]))
+    print(multiply_two_numbers(7, 9))
 
 
 
