9c74f2d46e81ee85565c70893c6d37c0ffa15678
Your Name <you@example.com>
2025-09-24 22:36:38 +0300

    Homework_logging

--- project/revision/function.py
+++ project/revision/function.py
@@ -10,3 +10,12 @@ def add_salt_to_list(given_list: list) -> None:
     id = uuid4().hex
     print(id)
     given_list.append(id)
+
+
+def get_string_length_with_no_whitespaces(string: str) -> int:
+    string_no_whitespaces = string.replace(" ", "")
+    return len(string_no_whitespaces)
+
+def get_auto_distance(speed: float, time: float) -> float:
+    dictance =  speed * time
+    return dictance
---
+++ project/tests/test_utils.py
@@ -0,0 +1,26 @@
+from revision.function import is_number_bigger_then_given, get_string_length_with_no_whitespaces, get_auto_distance
+
+def test_is_number_bigger_then_given_1():
+    given_number = 5
+    result = is_number_bigger_then_given(given_number)
+    expected_result = False
+    assert result == expected_result
+
+
+def test_1():
+    pass
+
+
+def test_get_string_length_with_no_whitespaces():
+    given_string = ""
+    expected_result = 0
+    actual_result = get_string_length_with_no_whitespaces(given_string)
+    assert actual_result == expected_result
+
+
+def test_get_auto_distance():
+    given_speed = 80
+    given_time = 6
+    actual_result = get_auto_distance(speed=given_speed, time=given_time)
+    expectet_result = 480
+    assert actual_result == expectet_result
