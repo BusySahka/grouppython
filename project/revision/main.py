ca766d8c69269808abbece098f54b522f6828928
Your Name <you@example.com>
2025-10-02 19:09:47 +0300

    homework11

--- project/revision/main.py
+++ project/revision/main.py
@@ -3,8 +3,12 @@ from project.revision.function import add_salt_to_list
 from hw_functions_utils import return_hello_buddy, return_biggest_num_from_array, multiply_two_numbers
 from revision.utils import say_hello
 from utils import greet_person, is_even, reverse_string, calculate_average, add_person_to_list, count_vowels, fahrenheit_to_celsius, say_hello
+from homeworks.homework11 import return_as_dictionary, calculate_sum, greet_person, multiply_numbers
 
 def main():
+    print(calculate_sum(11, 17))
+    print(greet_person("Sashka"))
+    print(multiply_numbers(5, 7, 9))
     #result = is_number_bigger_then_given(candidate_number=5)
     #print(result)
     #result = is_number_bigger_then_given(candidate_number=5, theshold=1)
@@ -12,14 +16,15 @@ def main():
     #print(return_hello_buddy())
     #print(return_biggest_num_from_array([10, "a", 5.5, None, 22, 5, "d"]))
     #print(multiply_two_numbers(7, 9))
-    print(greet_person("Олександра"))
-    print(is_even(16))
-    print(reverse_string("Привіт"))
-    print(calculate_average([1, 2, 3, 4, 5]))
-    print(add_person_to_list(["Олена", "Вікторія"], "Саша"))
-    print(count_vowels("Як ти, друже?"))
-    print(fahrenheit_to_celsius(1000))
-    print(say_hello("Олександра"))
+    #print(greet_person("Олександра"))
+    #print(is_even(16))
+    #print(reverse_string("Привіт"))
+    #print(calculate_average([1, 2, 3, 4, 5]))
+    #print(add_person_to_list(["Олена", "Вікторія"], "Саша"))
+    #print(count_vowels("Як ти, друже?"))
+    #print(fahrenheit_to_celsius(1000))
+    #print(say_hello("Олександра"))
+
 
 
 if __name__ == "__main__":
