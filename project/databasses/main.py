af2a6cb1b5fa997fc3ba7827dd1e3268dc5c3d60
Your Name <you@example.com>
2025-10-19 20:17:46 +0300

    homework_commit

--- project/databasses/main.py
+++ project/databasses/main.py
@@ -9,29 +9,35 @@ with psycopg2.connect(
         port=5432,
 ) as connection:
     with connection.cursor() as cursor:
-        query = """
-            CREATE TABLE IF NOT EXISTS brand (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(50) NOT NULL UNIQUE
-            )
-        """
-        cursor.execute(query)
+        query_topics = """
+                    CREATE TABLE IF NOT EXISTS topics (
+                        id SERIAL PRIMARY KEY,
+                        title VARCHAR(100) NOT NULL UNIQUE,
+                        description TEXT
+                    );
+                """
+        cursor.execute(query_topics)
 
-        query2 = """
-            CREATE TABLE IF NOT EXISTS customer (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(75) NOT NULL
-            );
+        query_users = """
+                    CREATE TABLE IF NOT EXISTS users (
+                        id SERIAL PRIMARY KEY,
+                        username VARCHAR(50) NOT NULL UNIQUE,
+                        email VARCHAR(75) NOT NULL,
+                        registered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    );
+                """
+        cursor.execute(query_users)
 
-            CREATE TABLE IF NOT EXISTS car (
-                id SERIAL PRIMARY KEY,
-                model VARCHAR(75) NOT NULL,
-                cost INTEGER,
-                brand_id INTEGER REFERENCES brand(id),
-                customer_id INTEGER REFERENCES customer(id)
-            );
-        """
-        cursor.execute(query2)
+        query_posts = """
+                    CREATE TABLE IF NOT EXISTS posts (
+                        id SERIAL PRIMARY KEY,
+                        content TEXT NOT NULL,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        user_id INTEGER REFERENCES users(id),
+                        topic_id INTEGER REFERENCES topics(id)
+                    );
+                """
+        cursor.execute(query_posts)
 
 
 
@@ -44,5 +50,7 @@ with psycopg2.connect(
         port=5432,
 ) as connection:
     with connection.cursor() as cursor:
-        query_insert = 'INSERT INTO brand (name) VALUES (%s)'
-        cursor.execute(query_insert, ("BMW", ))
+        query_insert = 'INSERT INTO topics (title, description) VALUES (%s, %s)'
+        cursor.execute(query_insert, ("School", "All about solving math problems"))
+        cursor.execute(query_insert, ("Sport", "How not to lose motivation"))
+        cursor.execute(query_insert, ("Programming", "Is it really that hard?"))
